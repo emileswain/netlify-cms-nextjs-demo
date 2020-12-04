bf50ac50b22e7a1933043cf119530fb47d23e73f
emile.swain <emile.swain@gmail.com>
2020-12-04 14:13:50 +0000

    Basic Admin only setup to show "react_devtools_backend.js:2430 Warning: Failed prop type: invariant is not defined" Error

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# Basic Admin only setup.
+
+Local Netlify cms server.
+```
+ yarn run cms
+```
+Local website 
+```
+ yarn run dev
+```
+
+open http://localhost:3000/admin
+
+See error Warning: Failed prop type: invariant is not defined
+```
+react_devtools_backend.js:2430 Warning: Failed prop type: invariant is not defined
+    at x (http://localhost:3000/_next/static/chunks/1.js:468:66487)
+    at l (http://localhost:3000/_next/static/chunks/1.js:460:53983)
+    at http://localhost:3000/_next/static/chunks/1.js:426:204302
+    at Connect(Component) (http://localhost:3000/_next/static/chunks/1.js:305:26246)
+    at c (http://localhost:3000/_next/static/chunks/1.js:305:24168)
+```
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -4,13 +4,20 @@ import config from '../cms/cms-config'
 
 const AdminPage = () => {
 
+    // use of import ('netlify-cms-app') has been the only way to get Nextjs to gen the admin page
+    // without throwing window undefined error.
+    //
+    // I also don't know how to properly type the config file.
     useEffect(() => {
         (async () => {
 
             const CMS = (await import('netlify-cms-app')).default ;
+
+            // i'm using local netlify cms server for testing.
             if (location.hostname === "localhost" || location.hostname === "127.0.0.1")
                  config.local_backend = true;
 
+            // how to type this.
             CMS.init({config});
         })();
     }, []);
